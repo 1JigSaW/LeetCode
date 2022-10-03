029deba8de551043a46e9a105e3e92b3d890bf6f
1JigSaW <misho_1998@mail.ru>
2022-10-03 21:39:15 +0300

    234

---
+++ 0234_PalindromeLinkedList.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow, fast, prev = head, head, None
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        prev, slow, prev.next = slow, slow.next, None
+        while slow:
+            slow.next, prev, slow = prev, slow, slow.next
+        fast, slow = head, prev
+        while slow:
+            if fast.val != slow.val: return False
+            fast, slow = fast.next, slow.next
+        return True
+
+
+A = ListNode(1)
+A.next = ListNode(2)
+A.next.next = ListNode(3)
+A.next.next.next = ListNode(4)
+A.next.next.next.next = ListNode(3)
+A.next.next.next.next.next = ListNode(2)
+A.next.next.next.next.next.next = ListNode(1)
+print(Solution().isPalindrome(A))
