9ac6fca59c481501ed655c720cb6fafc1ddf6803
1JigSaW <misho_1998@mail.ru>
2022-10-07 23:31:31 +0300

    278, 704

--- 0278_FirstBadVersion.py
+++ 0278_FirstBadVersion.py
@@ -1,15 +1,16 @@
 class Solution(object):
-    def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        left, right = 0, n
-        while left < right:
+    def firstBadVersion(self, n: int) -> int:
+        left = 0
+        right = n
+        while left <= right:
             mid = left + (right - left) // 2
-            if isBadVersion(mid):
-                right = mid
-            else:
+            if isBadVersion(mid) == False:
                 left = mid + 1
-        return left
+            else:
+                right = mid - 1
+        if isBadVersion(mid) == False:
+            return mid + 1
+        else:
+            return mid
+
         
--- 0704_BinarySearch.py
+++ 0704_BinarySearch.py
@@ -1,17 +1,35 @@
 class Solution(object):
     def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        left, right = 0, len(nums) - 1
+        left = 0
+        right = len(nums) - 1
         while left <= right:
-            mid = left + (right - left) // 2
-            if nums[mid] == target:
-                return mid
-            if target < nums[mid]:
-                right = mid - 1
+            middle = left + (right - left) // 2
+            print(middle)
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] < target:
+                left = middle + 1
             else:
-                left = mid + 1
+                right = middle - 1
         return -1
+
+    def search(self, nums, target):
+        if not nums: 
+            return -1
+        mid = len(nums) // 2 
+        num = nums[mid]
+        if num == target:
+            return mid
+        elif num > target:
+            return self.search(nums[:mid], target)
+        else:
+            srch = self.search(nums[mid+1:], target)
+            if srch == -1:
+                return -1
+            return mid + 1 + srch
+            
+
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(Solution().search(nums, target))
