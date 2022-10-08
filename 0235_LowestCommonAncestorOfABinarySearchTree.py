e16fb4ecc55935a39385b76d5d45365445576d7f
1JigSaW <misho_1998@mail.ru>
2022-10-08 23:10:59 +0300

    704, 98

---
+++ 0235_LowestCommonAncestorOfABinarySearchTree.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if p.val < root.val > q.val:
+                root = root.left
+            elif p.val > root.val < q.val:
+                root = root.right
+            else:
+                print(root.val)
+                return root
+
+
+
+root = TreeNode(6)
+root.left = TreeNode(2)
+root.right = TreeNode(8)
+root.left.left = TreeNode(0)
+root.left.right = TreeNode(4)
+root.right.left = TreeNode(7)
+root.right.right = TreeNode(9)
+root.left.right.left = TreeNode(3)
+root.left.right.right = TreeNode(5)
+p = root.left
+q = root.right
+print(Solution().lowestCommonAncestor(root, p, q))
