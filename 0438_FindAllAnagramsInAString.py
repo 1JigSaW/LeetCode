a18ee2a3cbc3c0a935a46fe4f8a35f140996ec36
1JigSaW <misho_1998@mail.ru>
2022-10-14 20:45:23 +0300

    438, 424

---
+++ 0438_FindAllAnagramsInAString.py
@@ -0,0 +1,30 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        n = len(s)
+        m = len(p)
+        
+        p = Counter(p)                 
+        ans = []
+           
+        window = None
+        for i in range(0,n-m+1):
+            if i == 0: 
+                window = Counter(s[:m])
+            else:    
+                window[s[i-1]] -= 1s
+                window[s[i+m-1]] += 1
+            if len(window - p) == 0:
+                ans.append(i)
+                
+        return ans
+
+
+
+
+
+s = "cbaebabacd"
+p = "abc"
+print(Solution().findAnagrams(s, p))
