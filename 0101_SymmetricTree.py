b6b276e88448bcb13bdbc66f48070ca43d958588
1JigSaW <misho_1998@mail.ru>
2022-10-21 17:53:05 +0300

    update 101, 112

--- 0101_SymmetricTree.py
+++ 0101_SymmetricTree.py
@@ -7,38 +7,52 @@ class TreeNode:
         self.right = right
 
 class Solution:
+    # iteratively
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        stack = [root]
-        right = [root.right]
-        left = [root.left]
+        stack1 = [root.left]
         res1 = []
-        res2 = []
-        while right:
-            node = right.pop()
+        while stack1:
+            node = stack1.pop()
             if node:
+                stack1.append(node.left)
+                stack1.append(node.right)
                 res1.append(node.val)
-                right.append(node.right)
-                right.append(node.left)
             else:
                 res1.append(None)
-        while left:
-            node = left.pop()
+        stack2 = [root.right]
+        res2 = []
+        while stack2:
+            node = stack2.pop()
             if node:
+                stack2.append(node.right)
+                stack2.append(node.left)
                 res2.append(node.val)
-                left.append(node.left)
-                left.append(node.right)
             else:
                 res2.append(None)
-            
-        print(res1, res2)
         return res1 == res2
+
+    # recursively
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        
+        def Symmetric_check(root1, root2):
+
+            if root1 == None and root2 == None:
+                return True
+
+            if (root1 != None and root2 != None) and (root1.val == root2.val):
+                return Symmetric_check(root1.left, root2.right) and Symmetric_check(root1.right, root2.left)
+
+            return False
+
+        return Symmetric_check(root,root)
+
 
 root = TreeNode(1)
 root.right = TreeNode(2)
 root.left = TreeNode(2)
 root.right.left = TreeNode(3)
 root.right.right = TreeNode(4)
-root.left.left = TreeNode(4)
+root.left.left = TreeNode(3)
 root.left.right = TreeNode(3)
 
 S = Solution()
--- 0112_PathSum.py
+++ 0112_PathSum.py
@@ -7,12 +7,15 @@ class TreeNode:
         self.right = right
 
 class Solution:
+
+    # iteratively
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
         stack = [(root, root.val)]
         while stack:
             curr, val = stack.pop()
+            print(stack)
             if not curr.left and not curr.right and val == targetSum:
                 return True
             if curr.right:
@@ -21,14 +24,23 @@ class Solution:
                 stack.append((curr.left, val+curr.left.val))
         return False
 
+    # recursively
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None: 
+            return False
+        
+        if root.left is None and root.right is None and root.val == targetSum:
+            return True
+        
+        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum-root.val)
 
 root = TreeNode(4)
 root.right = TreeNode(7)
 root.left = TreeNode(2)
-root.left.right = TreeNode(3)
+root.left.right = TreeNode(14)
 root.left.left = TreeNode(1)
 root.right.left = TreeNode(6)
-root.right.right = TreeNode(9)
+root.right.right = TreeNode(5)
 targetSum = 20
 
 S = Solution()
